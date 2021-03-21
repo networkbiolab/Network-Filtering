10480fbaf3c2193671ba091ea77b83efe98e3c32
leomur <34037498+leomur@users.noreply.github.com>
2021-03-21 15:16:47 -0300

    Update bed2gffFlybase.py

--- Reference_Networks/bed2gffFlybase.py
+++ Reference_Networks/bed2gffFlybase.py
@@ -1,16 +1,15 @@
+#LMS
+#leandro.murgas@mayor.cl
+
 import sys
 from glob import glob
 
-bedFiles = glob("/home/leandro/Desktop/pre_WEoN/TFs_ChIP/W_L3_TF_ENCODE/*.bed")
-
-# with open(sys.argv[1]) as file:
-#     data = file.readlines()
+bedFiles = glob(sys.argv[1]) #Enter the path folder of the files .bed to convert
 
 for bed in bedFiles:
     TF = bed.split("/")[-1][:-4]
     if "_" in TF:
         TF = TF[:-2]
-    # print(TF)
     with open(bed) as file:
         data = file.readlines()
         for line in data:
@@ -18,4 +17,3 @@ for bed in bedFiles:
             if "chr" in sp_line[0]:
                 sp_line[0] = sp_line[0].replace("chr","")
             print(sp_line[0]+"\t"+TF+"\tTF_binding_site\t"+sp_line[1]+"\t"+sp_line[2]+"\t.\t.\t.\t"+"ID=;Name=;Dbxref=;library=;bound_moiety=unknow:"+TF)
-            # input(".")
